8e26d83bf9fa61e018331de749110d488a12b019
pasha <150541162+pashabelii@users.noreply.github.com>
2023-11-12 11:58:26 +0300

    initiated deployment of fleekxyz/lens-template

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  assetPrefix: './',
+  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: ' ',
+  },
+  domains: [
+      'ipfs.infura.io',
+      'statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com',
+      'lens.infura-ipfs.io',
+      '4everland.io',
+      ""
+    ],
+}
+
+module.exports = nextConfig
